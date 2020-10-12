b1ea04ab86ad5f5bbafa828cbd6a9d89dddace35
Violeta <vio.ayelen@gmail.com>
2020-10-12 11:00:07 -0300

    array

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Constantes
 
-const q = document.getElementById("input_buscador");
+const q = document.getElementById("input_buscador"); 
+
+const indiceContador = document.getElementById("indiceContador");
 
 const sectionListActividades = document.getElementById("section_lista_actividades");
 
@@ -16,13 +18,17 @@ const listActividadesActivas = document.getElementById("lista_actividades_activa
 
 const btnAgregar = document.getElementById("btn_agregar");
 
-const btnEliminar = document.getElementById("btn_eliminar");
+const btnEliminar = document.getElementById("btn_eliminar"); 
+
+//const btnEliminarAct = document.getElementById("btn_eliminar_act");
 
 const btnActivar = document.getElementById("btn_activar");
 
 const btnDesactivar = document.getElementById("btn_desactivar");
 
+
 // Funciones
+
 
 // Hacer visible
 
@@ -38,36 +44,65 @@ function noVisible(elemento) {
 
 // Agregar elementos a la lista
 
+let acti = [];
+
 btnAgregar.addEventListener("click", function () {
     visible(sectionListActividades);
-    addLista(q.value, listActividades);
+
+    const divCont = document.createElement('div');
+    divCont.classList.add("botones");
+
+    const lista = document.createElement('li');
+    lista.innerHTML = (q.value); 
+
+    const btnActi = document.createElement("button");
+    const btnEdit = document.createElement('button');
+    const btnElim = document.createElement('button');     
+
+    listActividades.appendChild(divCont);
+    divCont.appendChild(lista);
+    divCont.appendChild(btnActi);
+    divCont.appendChild(btnEdit);
+    divCont.appendChild(btnElim);
+
+    
+    let count = acti.push(divCont);
+    console.log(acti)  
+    
 });
 
-//agregar div en html
+// Agregar div en html
 
-function addLista(actividad, idDiv) {
-    let resultsHTML = " ";
-    resultsHTML += `<div id="${indiceContador}">
-                      <p class="">"${actividad}"</p> 
-                      <button type="submit">Eliminar</button>
-                      <button type="submit">Activar</button>
-                      <button type="submit">Editar</button>                                                         
-                  </div>`;
+function addLista(actividad) {
 
-    idDiv.innerHTML = resultsHTML;
+
+
+ 
+}
+
+
+
+
+
+// ELIMINAR 
+
+function eliminar(padre, hijo) {
+    let eliminarElemento = padre.removeChild(hijo);
 }
 
 // Eliminar toda la lista
 
 btnEliminar.addEventListener("click", function () {
     eliminar(ContlistActividades, listActividades);
-    noVisible(sectionListActividades);
-    noVisible(sectionActividadesActivas);
+    location.reload();
 });
 
-function eliminar(padre, hijo) {
-    let eliminarElemento = padre.removeChild(hijo);
-}
+// Eliminar actividad
+
+var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+var removed = myFish.splice(3, 1);
+
+
 
 // Activar toda la lista
 
