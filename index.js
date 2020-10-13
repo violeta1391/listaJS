096e29f143421b4f96a4942f0d81333ac5689251
Violeta <vio.ayelen@gmail.com>
2020-10-13 11:44:30 -0300

    activar, eliminar

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 const q = document.getElementById("input_buscador"); 
 
-const indiceContador = document.getElementById("indiceContador");
-
 const sectionListActividades = document.getElementById("section_lista_actividades");
 
 const ContlistActividades = document.getElementById("cont_lista_actividades");
@@ -20,34 +18,30 @@ const btnAgregar = document.getElementById("btn_agregar");
 
 const btnEliminar = document.getElementById("btn_eliminar"); 
 
-//const btnEliminarAct = document.getElementById("btn_eliminar_act");
-
 const btnActivar = document.getElementById("btn_activar");
-
-const btnDesactivar = document.getElementById("btn_desactivar");
 
 
 // Funciones
 
 
-// Hacer visible
+// Visible
 
 function visible(elemento) {
     elemento.classList.remove("noVisible");
 }
 
-// Hacer no visible
+// No visible
 
 function noVisible(elemento) {
     elemento.classList.add("noVisible");
 }
+
 
 // Agregar elementos a la lista
 
 let arrayAct = [];
 
 let contador = 0;
-
 
 btnAgregar.addEventListener("click", function () {
     visible(sectionListActividades);
@@ -64,9 +58,10 @@ function addActividad() {
         let actividad =' '
     
             actividad += `<div id="${contador}" class="listCont">
-                            <p>"${q.value}"</p>   
+                            <li id="liAct${contador}">${q.value}</li>   
                             <button onclick="eliminarAct(${contador})">Eliminar</button>  
-                            <button onclick="eliminarAct(${contador})">Editar</button>                         
+                            <button onclick="editarAct(${contador})">Editar</button> 
+                            <button onclick="activarActividades(${contador})">Activar</button>                        
                         </div>`
     
         divCont.innerHTML = actividad; 
@@ -88,23 +83,80 @@ function eliminar(padre, hijo) {
 // Eliminar toda la lista
 
 btnEliminar.addEventListener("click", function () {
-    eliminar(ContlistActividades, listActividades);
-    location.reload();
+    //eliminar(ContlistActividades, listActividades);
+
+    let removed = arrayAct.splice(0, arrayAct.length);
+
+    //location.reload();
 });
 
 // Eliminar actividad 
 
 function eliminarAct(contador) {
 
-    const eli = document.getElementById(`${contador}`);
+    let eli = document.getElementById(`${contador}`);
     noVisible(eli);
     let removed = arrayAct.splice(contador, 1);   
     console.log(arrayAct);  
 }
 
 
-// Activar toda la lista
+// Editar actividad 
+
+function editarAct(contador) {
+    nuevoP = prompt('Nuevo nombre de la actividad')
+    let edi = document.getElementById(`liAct${contador}`);
+    edi.innerHTML = nuevoP;
+}
+
+// Activar una actividad  - cambiar q.value 
+
+let arrayActActivas = [];
+
+function activarActividades() {
+
+    visible(sectionActividadesActivas);
+
+    contador = contador + 1;       
+
+    const divCont = document.createElement('div');
+    
+    let actividad =' '
+    
+    actividad += `<div id="${contador}" class="listCont">
+                        <li id="liAct${contador}">${q.value}</li> 
+                        <button onclick="eliminarAct(${contador})">Eliminar</button>  
+                        <button onclick="editarAct(${contador})">Editar</button>                      
+                    </div>`
+    
+    divCont.innerHTML = actividad; 
+    
+    listActividadesActivas.appendChild(divCont);
+
+    let count = arrayActActivas.push(actividad);    
+            
+    console.log(arrayActActivas); 
+}
+
+/*
+
+// Activar lista completa 
 
 btnActivar.addEventListener("click", function () {
     visible(sectionActividadesActivas);
+
+    //activarActividades();
+
+    //let arrayActActivas = arrayAct.slice();
+
+    //arrayActActivas.forEach(activarActividades);
+
+    //eliminar(ContlistActividades, listActividades);
+
+    let removed = arrayAct.splice(0, arrayAct.length);  
+
+    console.log(arrayAct);      
+
 });
+
+*/
